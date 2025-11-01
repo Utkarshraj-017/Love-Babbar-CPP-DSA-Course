bad6f21f9b58649fa5a2f4accd2204728495ce60
Utkarsh Raj <129948401+Utkarshraj-017@users.noreply.github.com>
2025-11-02 01:20:13 +0530

    Refactor merge_k_sorted_arrays with custom node class
    with comments

--- Lectures/Lecture_76/Lecture_Codes/merge_k_sorted_arrays_approach_1.cpp
+++ Lectures/Lecture_76/Lecture_Codes/merge_k_sorted_arrays_approach_1.cpp
@@ -1,52 +1,60 @@
 #include <bits/stdc++.h> 
 
-class Node {
+// Node structure class
+class node {
     public:
-    int data;
-    int row;
-    int col;
-
-    Node(int data, int row, int col) {
-        this->data = data;
-        this->row = row;
-        this->col = col;
-    }
+    
+      int data;    //elementData
+      int i;       //arrayNumber  - row
+      int j;       //elementIndex - column
+        
+      //constructor
+      node(int data, int row, int col){
+          this->data = data;              // used this->data , coz 2 variables named data unlike row, col
+          i = row;
+          j = col;
+      }
 };
 
-class Compare {
+
+ // compare elements' value of 2 nodes
+class compare{
     public:
-    bool operator() (Node* a, Node* b) {
-        return a->data > b->data;
-    }
+      bool operator() (node* a, node* b){
+          return a->data > b->data;      // checks if elemnt of node a is greater than b
+      }
+        
 };
 
-// TC = O(nklog(k))
-// SC = O(nk)
-
-vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k) {
-    // Write your code here. 
-    priority_queue<Node*, vector<Node*>, Compare> pq;
-
-    for (int i=0; i<k; i++) {
-        Node* temp = new Node(kArrays[i][0], i, 0);
-        pq.push(temp);
-    }
-
-    vector<int> ans;
-
-    while (!pq.empty()) {
-        Node* temp = pq.top();
-        pq.pop();
-
-        ans.push_back(temp->data);
-
-        int newRow = temp->row;
-        int newCol = temp->col;
-
-        if (newCol + 1 < kArrays[newRow].size()) {
-            Node* next = new Node(kArrays[newRow][newCol+1], newRow, newCol+1);
-            pq.push(next);
+vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k)
+{
+    priority_queue<node*, vector<node*>, compare> minHeap;  // Using custom node to store the array elemnt the the array;s number(row) and element's index(col)
+        
+        //step1: Insert first elemnts of all k arrays
+        for(int i=0; i<k; i++){
+            node* temp = new node(kArrays[i][0], i, 0);
+            minHeap.push(temp);
         }
-    }
+        
+        vector<int> ans;
+        
+        //step2
+        while(!minHeap.empty()){
+            
+            // push the value of top-node (min) into ans array and pop the top node
+            node* top = minHeap.top();
+            ans.push_back(top->data);
+            minHeap.pop();
+            
+            int i = top->i;
+            int j = top->j;
+            
+            //create and push new node of the next element of the array(i) of which the popped node was into min heap
+            if(j+1 < kArrays[i].size()) {
+                node* next = new node(kArrays[i][j+1], i, j+1);
+                minHeap.push(next);
+            }
+            
+        }
     return ans;
 }
